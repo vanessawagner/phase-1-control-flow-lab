0831e31bc663720bf3ad82619e0a21ea030890c8
Vanessa Wagner <vmwagner@gmail.com>
2022-05-17 12:04:40 -0500

    commit tree 3873e09f894380060cdfebff0ac664b426ff24b5

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+   if (someValue < 400) {
+     result = 'This one is on me!'
+  } else if (someValue > 2500) {
+      result = 'No can do.'  
+  } else if (someValue > 2000) {
+      result = 'I will gladly take your thirty bucks.'
+  } else if (someValue > 400) {
+    result = 'That will be twenty bucks.'
+  }
+
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city==='NYC'){
+  return 'Ok, sounds good.'
+} else {
+  return 'No go.'
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip =='generous'){
+    return 'Thank you so much.'
+  }
+  else if (tip == 'not as generous'){
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.'
+  }
 }
